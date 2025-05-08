c73869084b671a660f719204904407bbc17eba6b
mreisenhofer-web <martin.reisenhofer@weboffice.co.at>
2025-05-08 17:29:46 +0200

    [IMP] config.py: better repository resolution.

--- oerp_util/__manifest__.py
+++ oerp_util/__manifest__.py
@@ -3,7 +3,7 @@
 # pylint: disable=license-allowed
 {
     'name': 'Utils',
-    'version': '17.0.2.0.0',
+    'version': '17.0.2.1.0',
     'summary': 'Base Utils for Testing, Development and Deployment',
     'category': 'Base',
     'author': 'martin-reisenhofer',
