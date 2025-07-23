26db9b8ca0c3bff1ceb61afebd150e10846ceea0
mreisenhofer-web <martin.reisenhofer@weboffice.co.at>
2025-07-23 09:06:51 +0200

    [INIT]

---
+++ models/ir_http.py
@@ -0,0 +1,32 @@
+from werkzeug.exceptions import BadRequest
+
+import odoo
+from odoo import models
+from odoo.http import request
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _auth_method_automation_task(cls):
+        token = request.httprequest.headers['X-Automation-Token']
+        dbname = request.httprequest.headers['X-Automation-DB']
+
+        #check header
+        if not dbname:
+            raise BadRequest("Database not specified")
+        if not token:
+            raise BadRequest("Token not specified")
+
+        # check token
+        registry = odoo.registry(dbname)
+        with registry.cursor() as cr:
+            cr.execute("SELECT COUNT(id) FROM automation_task_token WHERE token = %s", (token,))
+            token = cr.fetchall()
+            if not token:
+                raise BadRequest("Token not found")
+            if request.session.uid:
+                raise BadRequest("There should no user been set")
+
+        return True
