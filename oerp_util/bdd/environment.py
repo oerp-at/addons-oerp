4fce5d6063130a6a0e6364366e43ce482bbdf801
Martin Reisenhofer <martin.reisenhofer@weboffice.co.at>
2026-01-19 09:13:13 +0100

    [IMP] implemented basic for bdd.

--- oerp_util/bdd/environment.py
+++ oerp_util/bdd/environment.py
@@ -1,38 +1,38 @@
-from behave import fixture, use_fixture
+import re
+import random
+import string
+from behave import use_fixture
 from odoo.tests.common import TransactionCase
 
+SANITIZE_FEATURE_NAME_REGEX = re.compile(r'[^a-zA-Z0-9_ ]')
 
-########################################################
-# wrappers
-########################################################
 
-# simple wrapper for test class
-def case_class_wrapper(context, cls):
-    cls.setUpClass()
-    context.tcls = cls
-    yield
-    cls.doClassCleanups()
-    context.tcls = None
-
-# wrapper for test instance
-def case_instance(context):
-    if hasattr(context, 'tcls'):
-        context.tc = context.tcls()
-        context.tc.setUp()
-        context.env = context.tc.env
-        yield
-        context.tc.doCleanups()
-        context.env = None
-    else:
-        yield
+def feature_to_class(feature_name):
+    salt = ''.join([random.choice(string.ascii_uppercase)] + [random.choice(string.ascii_lowercase) for _ in range(4)])
+    sanitized_name = SANITIZE_FEATURE_NAME_REGEX.sub('', feature_name)
+    truncated_name = sanitized_name.split(' ')[0][:20]
+    truncated_name = truncated_name[0].upper() + truncated_name[1:]
+    class_name = truncated_name + salt
+    return class_name
 
 
 ########################################################
 # fixtures
 ########################################################
 
-def transaction_case_class(context):
-    yield from case_class_wrapper(context, TransactionCase)
+def odoo_test_class(context):
+    context.tcls = type(context.tcls_name, (context.tcls_base,), {})
+    context.tcls.setUpClass()
+    context.env = context.tcls.env
+    yield
+    context.tcls.doClassCleanups()
+
+def odoo_test_case(context):
+    context.tc = context.tcls()
+    context.tc.setUp()
+    context.env = context.tc.env
+    yield
+    context.tc.doCleanups()
 
 
 ########################################################
@@ -40,7 +40,11 @@ def transaction_case_class(context):
 ########################################################
 
 def before_feature(context, feature):
-    use_fixture(transaction_case_class, context)
+    if not hasattr(context, 'tcls_name'):
+        context.tcls_name = feature_to_class(feature.name)
+    if not hasattr(context, 'tcls_base'):
+        context.tcls_base = TransactionCase
+    use_fixture(odoo_test_class, context)
 
 def before_scenario(context, scenario):
-    use_fixture(case_instance, context)
+    use_fixture(odoo_test_case, context)
